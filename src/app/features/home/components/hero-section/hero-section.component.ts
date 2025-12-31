0e2e00509dac267c58d6c6292f649af29cc40e0c
mmartin027 <malajmartin3@gmail.com>
2025-12-31 14:52:32 +0100

    homepage

--- src/app/features/home/components/hero-section/hero-section.component.ts
+++ src/app/features/home/components/hero-section/hero-section.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'hero-section',
   imports: [],
+  standalone: true,
   templateUrl: './hero-section.component.html',
   styleUrl: './hero-section.component.css',
 })
--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { HeroSectionComponent } from './components/hero-section/hero-section.component';
+import { QuickSearchComponent } from './components/quick-search/quick-search.component';
 
 @Component({
   selector: 'home',
-  imports: [],
+  imports: [HeroSectionComponent, QuickSearchComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
