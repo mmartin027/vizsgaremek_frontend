d3e3ff54b94035b6ec29211981e2e671c0ddf2a2
mmartin027 <malajmartin3@gmail.com>
2026-01-04 15:30:44 +0100

    hero

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeroSectionComponent } from './components/hero-section/hero-section.component';
 import { QuickSearchComponent } from './components/quick-search/quick-search.component';
+import { HeaderComponent } from '../../shared/components/header/header.component';
 
 @Component({
   selector: 'home',
-  imports: [HeroSectionComponent, QuickSearchComponent],
+  imports: [HeaderComponent,HeroSectionComponent, QuickSearchComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
 
 @Component({
-  selector: 'header',
-  imports: [],
+  selector: 'app-header',
+  standalone: true,
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
