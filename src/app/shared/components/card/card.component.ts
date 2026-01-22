de733ec1228eb5ab12388cd97515d5adf15debb8
mmartin027 <malajmartin3@gmail.com>
2026-01-22 12:32:35 +0100

    booking

--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -21,22 +21,14 @@ export class CardComponent {
   @Input() spot!: ParkingSpotDto;
   @Output() viewDetails = new EventEmitter<number>();
 
-  // Város kinyerése: elvágjuk a szóköznél és az első elemet adjuk vissza
-  get city(): string {
-    if (!this.spot?.address) return '';
-    return this.spot.address.split(' ')[0]; 
-  }
-
-  get street(): string {
-    if (!this.spot?.address) return '';
-    const parts = this.spot.address.split(' ');
-    return parts.slice(1).join(' ');
-  }
+  
 
   get featureList(): string[] {
     if (!this.spot?.features) return [];
     return this.spot.features.split(',').map(f => f.trim());
   }
+
+ 
 
   onBtnClick() {
     this.viewDetails.emit(this.spot.id);
